f90c102787a613c7ee7370180a4ad1d6068e6c39
Morgan Guimard <morgan.guimard@docdoku.com>
2025-05-12 10:23:39 +0200

    quiz1

--- src/ougcv1/Main.java
+++ src/ougcv1/Main.java
@@ -1,9 +1,80 @@
 package ougcv1;
+//package ougcv2; // KO (1 seul package)
+
+import pkg2.Animal;
+import pkg2.Voiture;
+import pkg2.test.Robot;
+
+// 1 seule class public (top level classes)
+
+// private: privé
+// protected: accès au travers du package ou instance classe mère
+// public: open bar
+// rien : "package protected"  accès au travers du package
+
+class Class1 {
+	static public int a = 0;
+	static private int b = 0;
+	static protected int c = 0;
+	static int d = 0;
+}
+
+class main {
+}
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
+	static int a;
+	static float b;
+	static String c;
+
+	public static void main(int[] args) {
+		System.out.println("int version");
 	}
 
-}
+	public static void main(char[] args) {
+		System.out.println("char version");
+	}
+
+	public static void main(Object[] args) {
+	}
+
+	public static void test(int i) {
+
+	}
+
+	public static void test(float i) {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("String version");
+
+		test(123);
+		test(123.0f);
+
+		System.out.println(a); // ? 0
+		System.out.println(b); // ? 0.0
+		System.out.println(c); // ? null
+
+		System.out.println("Chaine c : " + c);
+
+		System.out.println(c.length()); // NullPointerException
+		// A : 0
+		// B : compilation error
+		// C : Exception
+		// D : la réponse D
+
+		Main m = new Main();
+
+		System.out.println("Hello world");
+		Animal a = new Animal();
+		Voiture v = new Voiture();
+		Robot r = new Robot();
+
+		System.out.println(Class1.a);
+		System.out.println(Class1.c);
+		System.out.println(Class1.d);
+
+	}
+};
---
+++ src/pkg2/test/Robot.java
@@ -0,0 +1,5 @@
+package pkg2.test;
+
+public class Robot {
+
+}
